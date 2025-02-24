1d0ee90fa31bcf04f808e50010177f5fae101675
ricmaria <156622743+ricmaria@users.noreply.github.com>
2025-02-24 23:16:23 +0100

    5. GLTF loading - Faster Draw - Draw sorting
    Completed the paragraph 5. GLTF loading - Faster Draw - Draw sorting

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1326,6 +1326,32 @@ void VulkanEngine::draw_geometry(VkCommandBuffer cmd)
 	//begin clock
 	auto start = std::chrono::system_clock::now();
 
+		// sort opaque surfaces
+
+	std::vector<uint32_t> opaque_draws;
+	opaque_draws.reserve(_mainDrawContext.OpaqueSurfaces.size());
+
+	for (uint32_t i = 0; i < _mainDrawContext.OpaqueSurfaces.size(); i++)
+	{
+		opaque_draws.push_back(i);
+	}
+
+	// sort the opaque surfaces by material and mesh
+	std::sort(opaque_draws.begin(), opaque_draws.end(),
+		[&](const auto& iA, const auto& iB)
+		{
+			const RenderObject& A = _mainDrawContext.OpaqueSurfaces[iA];
+			const RenderObject& B = _mainDrawContext.OpaqueSurfaces[iB];
+			if (A.material == B.material)
+			{
+				return A.indexBuffer < B.indexBuffer;
+			}
+			else
+			{
+				return A.material < B.material;
+			}
+		});
+
 	 // prepare GPU scene data descriptor set
 
 	// allocate a new uniform buffer for the scene data
@@ -1388,30 +1414,69 @@ void VulkanEngine::draw_geometry(VkCommandBuffer cmd)
 
 		// draw meshes
 
-	auto draw = 
-		[&](const RenderObject& draw)
-		{
-			vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, draw.material->pipeline->pipeline);
-			vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, draw.material->pipeline->layout, 0, 1, &globalDescriptor, 0, nullptr);
-			vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, draw.material->pipeline->layout, 1, 1, &draw.material->materialSet, 0, nullptr);
+	//defined outside of the draw function, this is the state we will try to skip
+	MaterialPipeline* lastPipeline = nullptr;
+	MaterialInstance* lastMaterial = nullptr;
+	VkBuffer lastIndexBuffer = VK_NULL_HANDLE;
 
-			vkCmdBindIndexBuffer(cmd, draw.indexBuffer, 0, VK_INDEX_TYPE_UINT32);
-
-			GPUDrawPushConstants pushConstants;
-			pushConstants.vertexBuffer = draw.vertexBufferAddress;
-			pushConstants.worldMatrix = draw.transform;
-			vkCmdPushConstants(cmd, draw.material->pipeline->layout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(GPUDrawPushConstants), &pushConstants);
-
-			vkCmdDrawIndexed(cmd, draw.indexCount, 1, draw.firstIndex, 0, 0);
-
-			//add counters for triangles and draws
-			_stats.drawcall_count++;
-			_stats.triangle_count += draw.indexCount / 3;
-		};
-
-	for (auto& r : _mainDrawContext.OpaqueSurfaces)
+	auto draw = [&](const RenderObject& r)
 	{
-		draw(r);
+		if (r.material != lastMaterial)
+		{
+			lastMaterial = r.material;
+			//rebind pipeline and descriptors if the material changed
+			if (r.material->pipeline != lastPipeline)
+			{
+				lastPipeline = r.material->pipeline;
+				vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, r.material->pipeline->pipeline);
+				vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, r.material->pipeline->layout, 0, 1, &globalDescriptor, 0, nullptr);
+
+				VkViewport viewport = {};
+				viewport.x = 0;
+				viewport.y = 0;
+				viewport.width = (float)_windowExtent.width;
+				viewport.height = (float)_windowExtent.height;
+				viewport.minDepth = 0.f;
+				viewport.maxDepth = 1.f;
+
+				vkCmdSetViewport(cmd, 0, 1, &viewport);
+
+				VkRect2D scissor = {};
+				scissor.offset.x = 0;
+				scissor.offset.y = 0;
+				scissor.extent.width = _windowExtent.width;
+				scissor.extent.height = _windowExtent.height;
+
+				vkCmdSetScissor(cmd, 0, 1, &scissor);
+			}
+
+			vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, r.material->pipeline->layout, 1, 1, &r.material->materialSet, 0, nullptr);
+		}
+		
+		//rebind index buffer if needed
+		if (r.indexBuffer != lastIndexBuffer)
+		{
+			lastIndexBuffer = r.indexBuffer;
+			vkCmdBindIndexBuffer(cmd, r.indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		}
+
+		// calculate final mesh matrix
+		GPUDrawPushConstants push_constants;
+		push_constants.worldMatrix = r.transform;
+		push_constants.vertexBuffer = r.vertexBufferAddress;
+
+		vkCmdPushConstants(cmd, r.material->pipeline->layout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(GPUDrawPushConstants), &push_constants);
+
+		vkCmdDrawIndexed(cmd, r.indexCount, 1, r.firstIndex, 0, 0);
+		
+		//stats
+		_stats.drawcall_count++;
+		_stats.triangle_count += r.indexCount / 3;
+	};
+
+	for (auto& r : opaque_draws)
+	{
+		draw(_mainDrawContext.OpaqueSurfaces[r]);
 	}
 
 	for (auto& r : _mainDrawContext.TransparentSurfaces)
